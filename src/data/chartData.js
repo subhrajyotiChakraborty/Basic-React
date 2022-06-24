f635da443f259c6e957ffe79202fbc38b2d2a8b9
Subhrajyoti <subha.ionic3@gmail.com>
2022-06-24 17:54:22 +0530

    removing extra color field

--- src/data/chartData.js
+++ src/data/chartData.js
@@ -4,32 +4,26 @@ const data = [
   {
     date: 1493922600000,
     units: 32,
-    color: "#E38627",
   },
   {
     date: 1494009000000,
     units: 55,
-    color: "#E3b4w2",
   },
   {
     date: 1494095400000,
     units: 32,
-    color: "#E38627",
   },
   {
     date: 1494181800000,
     units: 41,
-    color: "#E38627",
   },
   {
     date: 1494268200000,
     units: 21,
-    color: "#E38627",
   },
   {
     date: 1494354600000,
     units: 45,
-    color: "#E38627",
   },
 ];
 
